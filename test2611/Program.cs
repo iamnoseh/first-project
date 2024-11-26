044a1350cacb38f1b463712a4f25a9911aa454ef
Noseh Taghaymurodzoda <noseh.tagaymurodzoda@gmail.com>
2024-11-26 17:42:14 +0500

    test2

--- test2611/Program.cs
+++ test2611/Program.cs
@@ -3,5 +3,11 @@
     public int Id { get; set;}
     public string Name { get; set;}
     public int Age { get; set;}
-    
+
+}
+public class Person2
+{
+        public int Id { get; set;}
+        public string name { get; set;}
+        public string description { get; set;}
 }
